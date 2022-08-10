82f9c30a6b0648492023ab7ada51da8aee84dd8b
DavidVentura <davidventura27@gmail.com>
2022-08-10 10:02:18 +0200

    start parsing DB + reorganize header parsing

--- src/redis.rs
+++ src/redis.rs
@@ -1,4 +1,4 @@
-//use byteorder::ReadBytesExt;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 use std::error::Error;
 use std::iter::Peekable;
 
@@ -15,8 +15,22 @@ struct ProblemData {
     requirements: Requirements,
 }
 
+#[derive(Debug)]
 struct AuxHeader {
-    ver: String,
+    entries: Vec<RedisValues>,
+}
+
+#[derive(Debug)]
+struct Snapshot {
+    header: AuxHeader,
+    dbs: Vec<Database>,
+}
+
+#[derive(Debug)]
+struct Database {
+    id: u8,
+    entries: Vec<RedisValues>,
+    expiries: Vec<u32>,
 }
 
 #[derive(Debug)]
@@ -26,6 +40,13 @@ enum RedisValues {
     U32(String, u32),
 }
 
+#[derive(IntoPrimitive, TryFromPrimitive)]
+#[repr(u8)]
+enum OpCodes {
+    Aux = 0xFA,
+    SelectDB = 0xFE,
+    ResizeDB = 0xFB,
+}
 const RDB_ENCVAL: u8 = 0x3;
 
 const RDB_6BITLEN: u8 = 0x0;
@@ -39,51 +60,44 @@ fn read_key_value<I>(buf: &mut Peekable<I>) -> RedisValues
 where
     I: Iterator<Item = u8>,
 {
-    match buf.peek() {
-        Some(0xFA) => {
-            buf.next(); // consume the peek'd position
-            let key_len = buf.next().unwrap();
-            let key = String::from_utf8(buf.take(key_len as usize).collect()).unwrap();
-            // println!("key {}", key);
-            let hint = (buf.peek().unwrap() & 0xC0) >> 6;
-            // println!("peek {} hint {}", buf.peek().unwrap(), hint);
-            match hint {
-                RDB_ENCVAL => {
-                    let len = buf.next().unwrap() & 0x3F; // last 6 bits
-                                                          // isencoded now
-                    match len {
-                        RDB_ENC_INT8 => RedisValues::U8(key, buf.next().unwrap()),
-                        RDB_ENC_INT16 => panic!(),
-                        RDB_ENC_INT32 => {
-                            let bytes: Vec<u8> = buf.take(4).collect();
-                            assert_eq!(bytes.len(), 4);
-                            RedisValues::U32(
-                                key,
-                                u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
-                            )
-                        }
-                        _ => panic!(),
-                    }
-                }
-                RDB_6BITLEN => {
-                    let len = buf.next().unwrap() & 0x3F; // last 6 bits
-                                                          // RedisValues::U8(buf.next().unwrap())
-                                                          // FIXME: stringy bit
-                    let val = String::from_utf8(buf.take(len as usize).collect()).unwrap();
-                    RedisValues::STR(key, val)
-                }
-                RDB_14BITLEN => {
-                    // stringy ?
-                    let val_len =
-                        (((buf.next().unwrap() & 0x3F) as u32) << 8) | buf.next().unwrap() as u32;
-                    println!("val len {}", val_len);
-                    let val = String::from_utf8(buf.take(val_len as usize).collect()).unwrap();
-                    RedisValues::STR(key, val)
+    let key_len = buf.next().unwrap();
+    let key = String::from_utf8(buf.take(key_len as usize).collect()).unwrap();
+    // println!("key {}", key);
+    let hint = (buf.peek().unwrap() & 0xC0) >> 6;
+    // println!("peek {} hint {}", buf.peek().unwrap(), hint);
+    match hint {
+        RDB_ENCVAL => {
+            let len = buf.next().unwrap() & 0x3F; // last 6 bits
+                                                  // isencoded now
+            match len {
+                RDB_ENC_INT8 => RedisValues::U8(key, buf.next().unwrap()),
+                RDB_ENC_INT16 => panic!(),
+                RDB_ENC_INT32 => {
+                    let bytes: Vec<u8> = buf.take(4).collect();
+                    assert_eq!(bytes.len(), 4);
+                    RedisValues::U32(
+                        key,
+                        u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
+                    )
                 }
                 _ => panic!(),
             }
         }
-        _ => panic!("idk what to do"),
+        RDB_6BITLEN => {
+            let len = buf.next().unwrap() & 0x3F; // last 6 bits
+                                                  // RedisValues::U8(buf.next().unwrap())
+                                                  // FIXME: stringy bit
+            let val = String::from_utf8(buf.take(len as usize).collect()).unwrap();
+            RedisValues::STR(key, val)
+        }
+        RDB_14BITLEN => {
+            // stringy ?
+            let val_len = (((buf.next().unwrap() & 0x3F) as u32) << 8) | buf.next().unwrap() as u32;
+            println!("val len {}", val_len);
+            let val = String::from_utf8(buf.take(val_len as usize).collect()).unwrap();
+            RedisValues::STR(key, val)
+        }
+        _ => panic!(),
     }
 }
 
@@ -92,20 +106,48 @@ impl AuxHeader {
     where
         I: Iterator<Item = u8>,
     {
+        let mut entries = vec![];
         loop {
-            match buf.peek() {
-                Some(0xFE) => break,
-                Some(val) => {
+            match OpCodes::try_from(*buf.peek().unwrap()) {
+                Ok(OpCodes::SelectDB) => break, // AuxHeader ends on SelectDB
+                Ok(OpCodes::Aux) => {
+                    buf.next(); // consume the peek'd position
                     let ver = read_key_value(buf);
-                    println!("{:?}", ver);
+                    entries.push(ver);
                 }
-                None => panic!("ran out"),
+                Ok(_) => panic!("something else"),
+                Err(_) => panic!("ran out"),
             }
         }
-        None
+        Some(AuxHeader { entries })
     }
 }
 
+impl Database {
+    fn parse<I>(buf: &mut Peekable<I>) -> Option<Database>
+    where
+        I: Iterator<Item = u8>,
+    {
+        if *buf.peek().unwrap() != 0xFE {
+            return None;
+        }
+        buf.next(); // consume header
+        let db_id = buf.next().unwrap();
+        println!("db id {}", db_id);
+
+        loop {
+            match OpCodes::try_from(*buf.peek().unwrap()) {
+                Ok(OpCodes::ResizeDB) => break,
+                _ => panic!(),
+            }
+        }
+        Some(Database {
+            id: db_id,
+            entries: vec![],
+            expiries: vec![],
+        })
+    }
+}
 pub fn solve(parsed_data: String) -> Result<String, Box<dyn Error>> {
     let json_data: ProblemData = serde_json::from_str(&parsed_data)?;
     let mut rdb = base64::decode(json_data.rdb).unwrap();
@@ -120,6 +162,13 @@ pub fn solve(parsed_data: String) -> Result<String, Box<dyn Error>> {
     assert!(rdb.starts_with("REDIS".as_bytes()));
     rdb = rdb.split_off(9);
 
-    AuxHeader::parse(&mut rdb.into_iter().peekable());
+    let mut buf = rdb.into_iter().peekable();
+    let header = AuxHeader::parse(&mut buf);
+    let db = Database::parse(&mut buf);
+    let s = Snapshot {
+        header: header.unwrap(),
+        dbs: vec![db.unwrap()],
+    };
+    println!("{:#?}", s);
     Err("asd".into())
 }
